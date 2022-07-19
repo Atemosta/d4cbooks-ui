8156c7f605fcb598bfab2df7c1081c1261fd0bb9
Ousikai <mtfallsVR@protonmail.com>
2022-07-19 16:42:06 -0400

    dark mode monkaS pt4

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useContext, createContext } from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import ToggleColorMode from './components/ToggleColorMode';
 
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
+
 function App() {
+  const colorMode = useContext(ColorModeContext);
   const [mode, setMode] = useState('light');
   const theme = useMemo(
     () =>
@@ -15,12 +18,15 @@ function App() {
   );
 
   return (
-    <ThemeProvider theme={theme}>
-      <main>
-        <ToggleColorMode mode={mode} setMode={setMode}/>
-        <p>Mode: {mode}</p>
-      </main>
-    </ThemeProvider>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <main>
+          <ToggleColorMode mode={mode} setMode={setMode}/>
+          <p>Mode: {mode}</p>
+        </main>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+
   );
 }
 
